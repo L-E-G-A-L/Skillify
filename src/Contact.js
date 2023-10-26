c7c4609718e354b3e410f133721755f38a514114
Sai Swetha Tadivaka <saiswethatadivaka@gmail.com>
2023-10-25 19:55:17 -0500

    Contact Page Validations

--- src/Contact.js
+++ src/Contact.js
@@ -2,7 +2,70 @@ import React from "react";
 import "./css/contact.css";
 
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      phone: "",
+      email: "",
+      message: "",
+      errors: {
+        firstName: "",
+        lastName: "",
+        phone: "",
+        email: "",
+        message: "",
+      },
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const errors = {};
+
+    if (this.state.firstName.trim() === "") {
+      errors.firstName = "First name is required";
+    }
+
+    if (this.state.lastName.trim() === "") {
+      errors.lastName = "Last name is required";
+    }
+
+    if (this.state.phone.trim() === "") {
+      errors.phone = "Phone number is required";
+    } else if (!/^\d{10}$/.test(this.state.phone)) {
+      errors.phone =
+        "Phone number should be 10 digits and contain only numbers";
+    }
+
+    if (this.state.email.trim() === "") {
+      errors.email = "Email is required";
+    } else if (!/^\S+@\S+\.\S+$/.test(this.state.email)) {
+      errors.email = "Invalid email address";
+    }
+
+    if (this.state.message.trim() === "") {
+      errors.message = "Message is required";
+    }
+
+    if (Object.keys(errors).length === 0) {
+      console.log("Form submitted:", this.state);
+    } else {
+      this.setState({ errors });
+    }
+  };
+
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="contactClass">
         <header className="contactHeaderClass">
@@ -46,18 +109,54 @@ class Contact extends React.Component {
           <div className="content">
             <div className="form-content">
               <div className="topic-text">Get In Touch With Us</div>
-              <form action="#">
+              <form onSubmit={this.handleSubmit}>
                 <div className="input-box">
-                  <input type="text" placeholder="Enter your first name" />
+                  <input
+                    type="text"
+                    placeholder="Enter your first name"
+                    name="firstName"
+                    value={this.state.firstName}
+                    onChange={this.handleInputChange}
+                  />
+                  {errors.firstName && (
+                    <div className="error-message">{errors.firstName}</div>
+                  )}
                 </div>
                 <div className="input-box">
-                  <input type="text" placeholder="Enter your last name" />
+                  <input
+                    type="text"
+                    placeholder="Enter your last name"
+                    name="lastName"
+                    value={this.state.lastName}
+                    onChange={this.handleInputChange}
+                  />
+                  {errors.lastName && (
+                    <div className="error-message">{errors.lastName}</div>
+                  )}
                 </div>
                 <div className="input-box">
-                  <input type="tel" placeholder="Enter your phone no" />
+                  <input
+                    type="tel"
+                    placeholder="Enter your phone no"
+                    name="phone"
+                    value={this.state.phone}
+                    onChange={this.handleInputChange}
+                  />
+                  {errors.phone && (
+                    <div className="error-message">{errors.phone}</div>
+                  )}
                 </div>
                 <div className="input-box">
-                  <input type="email" placeholder="Enter your email" />
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleInputChange}
+                  />
+                  {errors.email && (
+                    <div className="error-message">{errors.email}</div>
+                  )}
                 </div>
                 <div className="input-box message-box">
                   <textarea
@@ -67,10 +166,15 @@ class Contact extends React.Component {
                     rows="10"
                     placeholder="Send us a message"
                     required
+                    value={this.state.message}
+                    onChange={this.handleInputChange}
                   ></textarea>
+                  {errors.message && (
+                    <div className="error-message">{errors.message}</div>
+                  )}
                 </div>
                 <div className="contactSubmitButton">
-                  <input type="button" value="Submit" />
+                  <input type="submit" value="Submit" />
                 </div>
               </form>
             </div>
