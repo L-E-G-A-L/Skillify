7f0996c53ee5eaaa571b614db3e391cf1b34ba54
arvind <akmanutd01@yahoo.in>
2023-10-31 11:46:47 -0500

    user delete fnality

--- src/UserPopup.js
+++ src/UserPopup.js
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./UserPopup.css";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const UserPopup = ({ userDataType, onClose }) => {
   const [userData, setUserData] = useState([]);
   const [Data, setData] = useState(true);
+
+  const handleDelete = (user_id) => {
+    axios
+      .post("http://localhost/LRFAuth.php", {
+        action: "deleteUser",
+        user_id: user_id,
+      })
+      .then((response) => {
+        if (response.data.success) {
+          setUserData((prevData) => prevData.filter((user) => user.user_id !== user_id));
+        } else {
+          console.log(response.data.error);
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting user:", error);
+      });
+  };
 
   useEffect(() => {
     axios
@@ -23,7 +43,7 @@ const UserPopup = ({ userDataType, onClose }) => {
       .catch((error) => {
         console.error("Error fetching user data:", error);
       });
-  }, []);
+  }, [userDataType]);
 
   return (
     <div className="admin-user-popup">
@@ -37,19 +57,25 @@ const UserPopup = ({ userDataType, onClose }) => {
             <tr className="admin-user-popup-table-tr">
               <th className="admin-user-popup-table-th">User Name</th>
               <th className="admin-user-popup-table-th">User Email</th>
+              <th className="admin-user-popup-table-th">Actions</th>
             </tr>
           </thead>
           <tbody>
             {Data ? (
               userData.map((user, index) => (
-                <tr className="admin-user-popup-table-tr" key={index}>
+                <tr className="admin-user-popup-table-tr" key={user.user_id}>
                   <td className="admin-user-popup-table-td">{user.user_name}</td>
                   <td className="admin-user-popup-table-td">{user.user_email}</td>
+                  <td className="admin-user-popup-table-td admin-user-popup-table-delete-button">
+                    <button onClick={() => handleDelete(user.user_id)}>
+                      <FontAwesomeIcon icon={faTrash} className="fa fa-trash" />
+                    </button>
+                  </td>
                 </tr>
               ))
             ) : (
               <tr className="admin-user-popup-table-tr">
-                <td colSpan="2" className="admin-user-popup-table-td admin-user-popup-table-td-nda">No data available</td>
+                <td colSpan="3" className="admin-user-popup-table-td admin-user-popup-table-td-nda">No data available</td>
               </tr>
             )}
           </tbody>
