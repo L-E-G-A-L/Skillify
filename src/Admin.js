4a06b489c17490fb5bc9f68fc5041fc9f54ec109
arvind <akmanutd01@yahoo.in>
2023-11-01 00:42:08 -0500

    user perm fnality

--- src/Admin.js
+++ src/Admin.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import UserPopup from './UserPopup';
 import './Admin.css';
+import PermissionPopup from './PermissionPopup';
 
 function Admin() {
   const [showUserPopup, setShowUserPopup] = useState(false);
+  const [showPermissionsPopup, setShowPermissionsPopup] = useState(false);
   const [role, setRole] = useState('');
 
   const openUserPopup = (userType) => {
@@ -16,6 +18,16 @@ function Admin() {
     setRole('');
   };
 
+  const openPermissionsPopup = (userType) => {
+    setRole(userType);
+    setShowPermissionsPopup(true);
+  };
+
+  const closePermissionsPopup = () => {
+    setShowPermissionsPopup(false);
+    setRole('');
+  };
+  
   return (
     <div className="admin-page">
       <div className="admin-navbar">
@@ -23,14 +35,14 @@ function Admin() {
         <a href="login" className='admin-nav-a'>Sign Out</a>
       </div>
       <div className="admin-content">
-        <button className="admin-dashboard-button">Dashboard</button>
+        {/* <button className="admin-dashboard-button">Dashboard</button> */}
         <div className="boxes">
           <div className="box">
             <h2 className="admin-box-h2">STUDENT</h2>
             <div className="button-group">
               <button className='admin-btngrp-btn' onClick={() => openUserPopup('student')}>MANAGE STUDENTS</button>
               <button className='admin-btngrp-btn'>MANAGE COURSE</button>
-              <button className='admin-btngrp-btn'>MANAGE PERMISSIONS</button>
+              <button className='admin-btngrp-btn' onClick={() => openPermissionsPopup('student')}>MANAGE PERMISSIONS</button>
               <button className='admin-btngrp-btn'>VIEW REPORTS</button>
               <button className='admin-btngrp-btn'>VIEW PERFORMANCE</button>
             </div>
@@ -40,7 +52,7 @@ function Admin() {
             <div className="button-group">
               <button className='admin-btngrp-btn' onClick={() => openUserPopup('coordinator')}>MANAGE COORDINATOR</button>
               <button className='admin-btngrp-btn'>HANDLE COURSE</button>
-              <button className='admin-btngrp-btn'>MANAGE PERMISSIONS</button>
+              <button className='admin-btngrp-btn' onClick={() => openPermissionsPopup('coordinator')}>MANAGE PERMISSIONS</button>
               <button className='admin-btngrp-btn'>ASSIGN STUDENTS</button>
               <button className='admin-btngrp-btn'>REVIEWS</button>
             </div>
@@ -50,7 +62,7 @@ function Admin() {
             <div className="button-group">
               <button className='admin-btngrp-btn' onClick={() => openUserPopup('qa')}>MANAGE QA-OFFICER</button>
               <button className='admin-btngrp-btn'>ASSIGN COURSE</button>
-              <button className='admin-btngrp-btn'>HANDLE PERMISSIONS</button>
+              <button className='admin-btngrp-btn' onClick={() => openPermissionsPopup('qa')}>HANDLE PERMISSIONS</button>
               <button className='admin-btngrp-btn'>REPORTS</button>
               <button className='admin-btngrp-btn'>PERFORMANCE</button>
             </div>
@@ -63,6 +75,9 @@ function Admin() {
       </footer>
       {showUserPopup && (
         <UserPopup userDataType={role} onClose={closeUserPopup} />
+      )}
+      {showPermissionsPopup && (
+        <PermissionPopup userDataType={role} onClose={closePermissionsPopup} />
       )}
     </div>
   );
