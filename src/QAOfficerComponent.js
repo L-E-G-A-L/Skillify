3c9d680f5f5d5e8a597b62563fce87bdcb2f1e97
Sai Swetha Tadivaka <saiswethatadivaka@gmail.com>
2023-11-10 20:59:53 -0600

    chat latest fix

--- src/QAOfficerComponent.js
+++ src/QAOfficerComponent.js
@@ -1,93 +1,144 @@
-import React, { useEffect, useState } from "react";
-import ScrollToBottom from "react-scroll-to-bottom";
-import { v4 as uuidv4 } from "uuid";
+import React, { useState, useEffect, useRef } from "react";
+import { TextField, Button} from "@mui/material";
+import io from "socket.io-client";
+import { format } from "date-fns";
+import "./QAChat.css";
+import axios from 'axios';
 import { Footer, QANav } from "./QADash";
 
+const socket = io("http://localhost:3001");
 
-function PersonA({ socket }) {
-  const [currentMessage, setCurrentMessage] = useState("");
-  const [messageList, setMessageList] = useState([]);
+function PersonA() {
+  const scrollRef = useRef();
 
-  const sendMessage = async () => {
-    if (currentMessage !== "") {
-      const messageData = {
-        senderSocketId: socket.id,
-        userId: generateUserId(), // Function to generate unique user ID
-        message: currentMessage,
-        time: new Date(Date.now()).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }),
-      };
-
-      await socket.emit("send_message", messageData);
-      setMessageList((list) => [...list, messageData]);
-      setCurrentMessage("");
-    }
-  };
+  const [username, setUserName] = useState('');
+  const [message, setMessage] = useState("");
+  const [chatMessages, setChatMessages] = useState([]);
 
   useEffect(() => {
-    socket.on("receive_message", (data) => {
-      // Check if the received message socket.id is not the same as the current client's socket.id
-      if (data.senderSocketId !== socket.id) {
-        setMessageList((list) => [...list, data]);
-      }
-    });
-    // Clean up the event listener when the component unmounts
-    return () => {
-      socket.off("receive_message");
-    };
-  }, [socket]);
+    const userRole = sessionStorage.getItem('userRole');
+    const user_id = sessionStorage.getItem('userId');
 
-  const generateUserId = () => {
-    return uuidv4(); // Replace this with the actual generated user ID
+    // Scroll to bottom if new message received
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+
+    // Setup Socket Events
+    const setupEvents = () => {
+      socket.on("receive_message", (e) => {
+        const data = JSON.parse(e);
+
+        const lastMessage = chatMessages.length
+          ? chatMessages[chatMessages.length - 1]
+          : null;
+
+        if (
+          !(
+            lastMessage?.message === data.message &&
+            lastMessage?.createdDate === data.createdDate
+          )
+        ) {
+          chatMessages.push(data);
+          setChatMessages([...chatMessages]);
+        }
+      });
+    };
+
+    axios
+    .get(`https://sxt7404.uta.cloud/php/qausernamefetch.php?user_id=${user_id}&role=${userRole}`)
+    .then((response) => {
+      console.log(response.data);
+      if (response.data.users && response.data.users.user_name) {
+        const fetchedUserName = response.data.users.user_name;
+        setUserName(fetchedUserName);
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+    setupEvents();
+  }, [chatMessages]);
+
+  // Send Messge
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    const data = { username, message, createdDate: new Date() };
+
+    socket.emit("send_message", JSON.stringify(data));
+
+    setMessage("");
   };
 
-  return (
-    <html lang="en">
-    <body className="discusuionbody">
-    <QANav title="Chat" />
-    <div className="chat-window">
-      <div className="chat-header">
-        <p>QA Chat</p>
-      </div>
-      <div className="chat-body">
-        <ScrollToBottom className="message-container">
-          {messageList.map((messageContent, index) => (
-            <div
-              className="message"
-              key={index}
-              id={messageContent.senderSocketId === socket.id ? "you" : "other"}
-            >
-              <div className="wwww">
-                <div className="message_content">
-                  <p>{messageContent.message}</p>
-                </div>
-                <div className="message-meta">
-                  <p id="time">{messageContent.time}</p>
-                  {/* <p id="author">{messageContent.userId}</p> */}
-                </div>
-              </div>
+  // Welcome User message
+  const welcomeMessageView = () => (
+    <div className="welcome-chat-container">
+      Welcome {username}
+    </div>
+  );
+
+  // Messages View
+
+  const messagesView = () => (
+    <div ref={scrollRef} className="message-chat-container">
+      {chatMessages?.map(({ username: otherUsername, message, createdDate }, index) => {
+        const self = otherUsername === username;
+  
+        return (
+          <div key={username + index} className={`message-grid ${self ? 'message-self' : 'message-other'}`}>
+            <div className={`message-text ${self ? 'textRight' : 'textLeft'}`}>
+              {otherUsername}
             </div>
-          ))}
-        </ScrollToBottom>
-      </div>
-      <div className="chat-footer">
-        <input
-          type="text"
-          value={currentMessage}
-          placeholder="Type your message"
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
-          onKeyDown={(event) => {
-            event.key === "Enter" && sendMessage();
+            <div className={`message-typography ${self ? 'message-self-typography' : 'message-other-typography'}`}>
+              {message}
+            </div>
+            <div className={`message-text ${self ? 'textRight' : 'textLeft'}`}>
+              {format(new Date(createdDate), 'hh:mm a')}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+
+  // Send Message Input
+  const controlsView = () => (
+    <div className="controls-container">
+      <div className="text-field">
+        <TextField
+          autoFocus
+          variant="standard"
+          fullWidth
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          InputProps={{
+            disableUnderline: true,
           }}
         />
-        <button className="send_button" onClick={sendMessage}>&#9658;</button>
+      </div>
+      <div className="send-chat-button">
+        <Button type="submit">Send</Button>
+      </div>
+    </div>
+  );
+
+  return (
+    <form onSubmit={sendMessage} className="form">
+    <QANav title="Chat" />
+    <div className=".discussion-container">
+      <div className="dialogContainer">
+        {welcomeMessageView()}
+
+        {messagesView()}
+
+        {controlsView()}
       </div>
     </div>
     <Footer />
-    </body>
-  </html>
-  );
+  </form>
+);
 }
 
 export default PersonA;
